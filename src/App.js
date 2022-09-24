8a8c36c91c60064a61be81a433c11cffc7293a05
geetikamahajan <geetika00@gmail.com>
2022-09-24 16:12:58 -0400

    Create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from "./Item";
 
 function App() {
+
+    const topSizes = ["XS", "S", "M"]
+    const bottomSizes = ["27\"", "30\"", "31\""]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Happy birthday Mayank!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>
+          Scroll down and pick out a gift :)
+      </p>
       </header>
+        <div className="Items-container">
+            <Item name={"Textured Knit Crewneck Sweater (Heathered Natural Ivory)"}
+                  sizes={topSizes}
+                  lengths={[]}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM3DQBS_050847_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+            <Item name={"Surge Jogger (Dark Olive)"}
+                  sizes={topSizes}
+                  lengths={bottomSizes}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM5956S_026083_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+            <Item name={"Engineered Warmth Long Sleeve Crew (Vapor/Gull Grey/Black)"}
+                  sizes={topSizes}
+                  lengths={[]}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM3CDES_056949_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+            <Item name={"Engineered Warmth Long Sleeve Crew (Natural Ivory/Natural Ivory)"}
+                  sizes={topSizes}
+                  lengths={[]}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM3CLLS_049872_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+            <Item name={"Surge Jogger (Black)"}
+                  sizes={topSizes}
+                  lengths={bottomSizes}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM5956S_0001_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+            <Item name={"GridLiner Fleece Jogger (Heathered Black)"}
+                  sizes={topSizes}
+                  lengths={[]}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM5AF7S_1966_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+            <Item name={"GridLiner Fleece Jogger (Heathered Dove Grey)"}
+                  sizes={topSizes}
+                  lengths={[]}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM5AF7S_033089_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+            <Item name={"City Sweat Crew (White Opal)"}
+                  sizes={topSizes}
+                  lengths={[]}
+                  imgLink={"https://images.lululemon.com/is/image/lululemon/LM3BUGS_047748_1?wid=1600&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72"}></Item>
+
+        </div>
     </div>
   );
 }
---
+++ src/Item.js
@@ -0,0 +1,117 @@
+import './Item.css';
+import * as React from 'react';
+
+import {FormControl, InputLabel, Select, MenuItem, Button} from "@mui/material";
+import emailjs from 'emailjs-com';
+
+function Item({name, imgLink, sizes, lengths}) {
+    const [itemSize, setItemSize] = React.useState('');
+    const [itemLength, setItemLength] = React.useState('');
+    const [buttonValue, setButtonValue] = React.useState('Tell Geetika I want this one');
+    const [buttonVariant, setButtonVariant] = React.useState('outlined');
+    const menuItemsLength = lengths.map(length => <MenuItem value={length}>{length}</MenuItem>)
+    const menuItemsSize = sizes.map(size => <MenuItem value={size}>{size}</MenuItem>)
+    const handleChangeSize = (event) => {
+        setItemSize(event.target.value);
+    };
+    const handleChangeLength = (event) => {
+        setItemLength(event.target.value);
+    };
+    function tellGeetika(e) {
+        e.preventDefault()
+        if (itemSize !== '' && buttonValue === 'Tell Geetika I want this one') {
+            setButtonValue("Told Geetika I want this one")
+            setButtonVariant('contained')
+            let combinedSize = itemSize;
+            if (itemLength !== '') {
+                combinedSize = combinedSize + "/" + itemLength;
+            }
+            const templateParams = {
+                item_name: name,
+                size: combinedSize
+            };
+            sendEmail(templateParams);
+        } else {
+            setButtonValue("Tell Geetika I want this one")
+            setButtonVariant('outlined')
+        }
+    }
+    function sendEmail(templateParams) {
+        emailjs.send('service_uenjcwb', 'template_eysfuug', templateParams, 'tCpV5PR_VWOgzH79x')
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    }
+
+    if (lengths.length === 0) {
+        return (
+            <div className={"Item"}>
+                <img className={"Image"} src={imgLink}/>
+                <p>{name}</p>
+                <div className={"Item-options"}>
+                    <div>
+                        <FormControl required variant={'outlined'} className={"Size-select"}>
+                            <InputLabel id="demo-simple-select-label">Size</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={itemSize}
+                                label="Size"
+                                onChange={handleChangeSize}
+                            >
+                                {menuItemsSize}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className={"Button"}>
+                        <Button variant={buttonVariant} onClick={tellGeetika}>{buttonValue}</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className={"Item"}>
+                <img className={"Image"} src={imgLink}/>
+                <p>{name}</p>
+                <div className={"Item-options"}>
+                    <div>
+                        <FormControl required variant={'outlined'} className={"Size-select"}>
+                            <InputLabel id="demo-simple-select-label">Size</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={itemSize}
+                                label="Size"
+                                onChange={handleChangeSize}
+                            >
+                                {menuItemsSize}
+                            </Select>
+                        </FormControl>
+                        <FormControl required variant={'outlined'} className={"Length-select"}>
+                            <InputLabel id="demo-simple-select-label">Length</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={itemLength}
+                                label="Length"
+                                onChange={handleChangeLength}
+                            >
+                                {menuItemsLength}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className={"Button"}>
+                        <Button variant={buttonVariant} onClick={tellGeetika}>{buttonValue}</Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+
+}
+
+export default Item;
